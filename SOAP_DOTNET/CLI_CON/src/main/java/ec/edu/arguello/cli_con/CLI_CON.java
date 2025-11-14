b243c702bd041ae70508b83473dcbfc9501e6ed3
Joel Daniel Arguello Espinosa <joel.darguello@gmail.com>
2025-11-13 20:13:05 -0500

    Agregar configuración y archivos iniciales para las aplicaciones CLI_CON, CLI_ESC y CLI_WEB, incluyendo archivos POM, clases Java y configuraciones de NetBeans.

---
+++ SOAP_DOTNET/CLI_CON/src/main/java/ec/edu/arguello/cli_con/CLI_CON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.arguello.cli_con;
+
+/**
+ *
+ * @author joelarguello
+ */
+public class CLI_CON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SOAP_DOTNET/CLI_ESC_COMERCIALIZADORA/src/main/java/ec/edu/arguello/cli_esc/CLI_ESC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.arguello.cli_esc;
+
+/**
+ *
+ * @author joelarguello
+ */
+public class CLI_ESC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
